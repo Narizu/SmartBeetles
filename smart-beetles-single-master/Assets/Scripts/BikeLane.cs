d1e155d163f04b2c2523bf244d12575642108c45
Narizu <salva.aparici@hotmail.com>
2018-02-27 15:26:01 +0100

    Revert "Revert "Añadidas imágenes para el carril bici""
    This reverts commit 2e13d6c13d2429fb0493edcc760bc8095648b961.

--- smart-beetles-single-master/Assets/Scripts/BikeLane.cs
+++ smart-beetles-single-master/Assets/Scripts/BikeLane.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BikeLane : MonoBehaviour
 {
 
+	public RawImage runImage;
+
 	private SphereControl sphere;
-	private float maxSpeed = 2f;
-	private float maxSpeedBike = 3f;
+	private float maxSpeed;
+	private float maxSpeedBike;
+
+	private void Start ()
+	{
+
+		runImage.enabled = false;
+		maxSpeed = 2f;
+		maxSpeedBike = 3f;
+
+	}
 
 	private void OnTriggerEnter (Collider other)
 	{
@@ -15,8 +27,9 @@ public class BikeLane : MonoBehaviour
 		if (other.gameObject.tag == "Sphere")
 		{
 			
+			runImage.enabled = true;
 			sphere = other.gameObject.GetComponent<SphereControl>();
-			sphere.SetMaxSpeed (maxSpeedBike);
+			sphere.SetMaxSpeed(maxSpeedBike);
 
 		}
 
@@ -28,8 +41,9 @@ public class BikeLane : MonoBehaviour
 		if (other.gameObject.tag == "Sphere")
 		{
 			
+			runImage.enabled = false;
 			sphere = other.gameObject.GetComponent<SphereControl>();
-			sphere.SetMaxSpeed (maxSpeed);
+			sphere.SetMaxSpeed(maxSpeed);
 
 		}
 
