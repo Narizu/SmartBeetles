67e1a8532a832ef893052a149f207cdb39f317c3
Narizu <salva.aparici@hotmail.com>
2018-04-10 19:41:55 +0200

    Update

---
+++ smart-beetles-single-master/Assets/Scripts/PointsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+
+
+	Text text;
+
+
+	// Use this for initialization
+	void Start () {
+
+		text = GetComponent<Text> ();
+
+
+		text.text = "Pruebecilla";
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
