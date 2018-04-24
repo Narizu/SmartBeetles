13f5ac437c2416ee880bb49d16b67f1888be685f
Narizu <salva.aparici@hotmail.com>
2018-04-24 19:06:29 +0200

    Partículas

---
+++ smart-beetles-single-master/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+	private SphereControl sphere;
+
+	void Start ()
+	{
+		
+		//if (sphere == null) {
+
+			sphere = GameObject.FindGameObjectWithTag ("Sphere").GetComponent<SphereControl> ();
+
+		//}
+
+	}
+
+	void Update ()
+	{
+		
+		if (sphere == null) {
+
+			sphere = GameObject.FindGameObjectWithTag ("Sphere").GetComponent<SphereControl> ();
+
+		}
+
+		transform.position = sphere.transform.position;
+
+	}
+
+}
